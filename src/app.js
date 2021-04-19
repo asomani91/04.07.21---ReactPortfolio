aeb524aea867d5515193225a36343492cfd85766
Ankit Somani <ankit.m.somani@gmail.com>
2021-04-18 22:07:54 -0400

    setting css, js & test for app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Layout from './Layout';
+import './App.css';
+
+function App() {
+    return (
+        <React.Fragment>
+            <Layout />
+        </React.Fragment>
+    );
+}
+
+export default App;
