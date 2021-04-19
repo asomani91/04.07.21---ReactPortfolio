bbb15b6ed23c3e34bbf651630e89f2ad78ee3230
Ankit Somani <ankit.m.somani@gmail.com>
2021-04-18 22:43:01 -0400

    setting cs & js for index

---
+++ src/layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Navbar, Intro, Project, Footer, About } from './Components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+export default function Layout() {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <Switch>
+                <Route exact path='/' component={Intro} />
+                <Route exact path='/about' component={About} />
+            </Switch>
+            <Project />
+            <Footer />
+        </React.Fragment>
+    )
+}
